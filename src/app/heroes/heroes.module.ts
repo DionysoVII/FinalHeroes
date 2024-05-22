978d59431738dcb104c84be5307fa3d1af625a96
Dionisos <luisksilvacontreras@gmail.com>
2024-05-22 10:35:06 -0500

    Heroes update

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,27 @@
+import { MaterialModule } from './../material/material.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { HeroPagesComponent } from './pages/heroPages/heroPages.component';
+import { LayoutPagesComponent } from './pages/layoutPages/layoutPages.component';
+import { ListPagesComponent } from './pages/listPages/listPages.component';
+import { NewPagesComponent } from './pages/newPages/newPages.component';
+import { SearchPagesComponent } from './pages/searchPages/searchPages.component';
+
+
+@NgModule({
+  declarations: [
+    HeroPagesComponent,
+    LayoutPagesComponent,
+    ListPagesComponent,
+    NewPagesComponent,
+    SearchPagesComponent
+  ],
+  imports: [
+    CommonModule,
+    HeroesRoutingModule,
+    MaterialModule
+  ]
+})
+export class HeroesModule { }
